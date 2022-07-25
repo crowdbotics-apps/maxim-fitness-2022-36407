0dd8db89b5a2161fab324ec3d952d9c1dca629bb
Crowdbotics <team@crowdbotics.com>
2022-07-25 18:09:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/maxim_fitness_2022_36407/MainActivity.java
@@ -0,0 +1,15 @@
+package com.maxim_fitness_2022_36407;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "maxim_fitness_2022_36407";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "maxim_fitness_2022_36407",
+  url: "https://maxim_fitness_2022_36407.botics.co",
+  api: "https://maxim_fitness_2022_36407.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
